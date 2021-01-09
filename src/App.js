cbc3f17f0a1a3e80c30dbd47e82739b7e7216788
Aldi Piero Ashady <aldipiero10@gmail.com>
2021-01-09 15:27:09 +0700

    added axios and styling

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomeScreen from "./views/HomeScreen";
+import ProductScreen from "./views/ProductScreen";
 
 function App() {
   return (
-    <>
+    <Router>
       <Header />
       <main className="py-3">
         <Container>
-          <HomeScreen />
+          <Route path="/" component={HomeScreen} exact />
+          <Route path="/product/:id" component={ProductScreen} exact />
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 }
 
--- src/views/HomeScreen.js
+++ src/views/HomeScreen.js
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Col, Row } from "react-bootstrap";
 import Product from "../components/Product";
-import products from "../products";
+import axios from "axios";
 
 function HomeScreen() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  async function fetchProducts() {
+    const { data } = await axios.get("/api/products");
+
+    setProducts(data);
+  }
+
   return (
     <>
       <h1>Latest Products</h1>
       <Row>
         {products.map((product) => (
-          <Col sm={12} lg={4} xl={3}>
+          <Col key={product._id} sm={12} lg={4} xl={3}>
             <Product product={product} />
           </Col>
         ))}
